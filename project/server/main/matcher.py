da82e9c4fc03ea54fd25e41afebd67a825dfe1ac
eric <eric.jeangirard@recherche.gouv.fr>
2022-02-23 18:26:56 +0100

    preprocess publi

--- project/server/main/matcher.py
+++ project/server/main/matcher.py
@@ -3,23 +3,43 @@ from project.server.main.idref_matcher import name_idref_match
 from project.server.main.strings import normalize
 from project.server.main.logger import get_logger
 
+import os
+import json
 import pymongo
 import pandas as pd
 
 logger = get_logger(__name__)
 MOUNTED_VOLUME = '/upw_data/'
 
-def pre_process_publications():
+def get_publications_from_author_key(author_key):
     myclient = pymongo.MongoClient('mongodb://mongo:27017/')
     mydb = myclient['scanr']
+    mycoll = mydb['person_matcher_input']
+    return list(mycoll.find({'authors.author_key': author_key}))
+
+
+def pre_process_publications(args):
+    logger.debug('dropping collection person_matcher_input')
+    myclient = pymongo.MongoClient('mongodb://mongo:27017/')
+    mydb = myclient['scanr']
+    mycoll = mydb['person_matcher_input']
+    mycoll.drop()
 
     df_all = pd.read_json(f'{MOUNTED_VOLUME}/test-scanr_full.jsonl', lines=True, chunksize=25000)
+    author_keys = []
     for df in df_all:
         publications = df.to_dict(orient='records')
-        prepare_publications(publications)
+        prepared = prepare_publications(publications)
+        save_to_mongo(prepared['relevant'])
+        author_keys += prepared['author_keys']
+        author_keys = list(set(author_keys))
+    logger.debug(f'{len(author_keys)} author_keys detected')
+    json.dump(author_keys, open(f'{MOUNTED_VOLUME}/author_keys.json', 'w'))
+    #return author_keys
 
 def prepare_publications(publications):
     relevant_infos = []
+    author_keys = []
     # keeping and enriching only with relevant info for person matching
     for p in publications:
         new_elt = {'id': p['title_first_author']}
@@ -36,26 +56,38 @@ def prepare_publications(publications):
                 current_author['last_name'] = a['last_name']
                 current_author['first_name'] = a['first_name']
             elif a.get('full_name'):
-                author_key = normalize(a.get('full_name', remove_space=True))
+                author_key = normalize(a.get('full_name'), remove_space=True)
                 current_author['full_name'] = a['full_name']
 
             if author_key and len(author_key) > 4:
                 current_author['author_key'] = author_key
                 entity_linked.append(author_key)
+                author_keys.append(author_key)
 
             if a.get('id'):
                 entity_linked.append(a['id'])
                 current_author['id'] = a['id']
             new_elt['authors'].append(current_author)
 
-        for other_entity in ['issns', 'keywords']:
-            for elt in p.get(other_entity, []):
+        
+        issns = p.get('journal_issns', '')
+        if isinstance(issns, str) and issns:
+            for elt in issns.split(','):
+                entity_linked.append(normalize(elt, remove_space=True))
+        
+        for other_entity in ['keywords']:
+            elts = p.get(other_entity, [])
+            if not isinstance(elts, list):
+                continue
+            for elt in elts:
                 entity_linked.append(normalize(elt, remove_space=True))
 
         entity_linked = list(set(entity_linked))
         new_elt['entity_linked'] = entity_linked
-    relevant_infos.append(new_elt)
-    
+        relevant_infos.append(new_elt)
+    return {'relevant': relevant_infos, 'author_keys': author_keys}
+
+def save_to_mongo(relevant_infos):
     myclient = pymongo.MongoClient('mongodb://mongo:27017/')
     mydb = myclient['scanr']
     output_json = f'{MOUNTED_VOLUME}person-matcher-current.jsonl'
@@ -70,8 +102,6 @@ def prepare_publications(publications):
     mycol.create_index('authors.author_key')
     logger.debug(f'Deleting {output_json}')
     os.remove(output_json)
-
-    return publis
 
 def match(publications, author_key):
 
--- project/server/main/views.py
+++ project/server/main/views.py
@@ -3,6 +3,7 @@ from rq import Queue, Connection
 from flask import render_template, Blueprint, jsonify, request, current_app
 
 from project.server.main.tasks import create_task_match
+from project.server.main.matcher import pre_process_publications
 
 main_blueprint = Blueprint("main", __name__,)
 from project.server.main.logger import get_logger
@@ -13,6 +14,22 @@ logger = get_logger(__name__)
 @main_blueprint.route("/", methods=["GET"])
 def home():
     return render_template("main/home.html")
+
+@main_blueprint.route("/preprocess", methods=["POST"])
+def run_task_preprocess():
+    args = request.get_json(force=True)
+    with Connection(redis.from_url(current_app.config["REDIS_URL"])):
+        q = Queue("person-matcher", default_timeout=216000)
+        task = q.enqueue(pre_process_publications, args)
+    response_object = {
+        "status": "success",
+        "data": {
+            "task_id": task.get_id()
+        }
+    }
+    return jsonify(response_object), 202
+
+
 
 @main_blueprint.route("/match2", methods=["POST"])
 def run_task_match2():
