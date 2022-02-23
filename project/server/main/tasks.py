afc3d2953b1f805f8c48aac87001c787260f208c
eric <eric.jeangirard@recherche.gouv.fr>
2022-02-23 15:00:39 +0100

    typo

--- project/server/main/tasks.py
+++ project/server/main/tasks.py
@@ -8,7 +8,7 @@ from project.server.main.logger import get_logger
 
 logger = get_logger(__name__)
 
-def create_task_match(arg):
+def create_task_match(args):
     publications = args.get('publications', [])
     author_key = args.get('author_key')
     return match(publications=publications, author_key=author_key)
