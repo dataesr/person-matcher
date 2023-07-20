e2464cc341f5f174aeb42b5caac368ebee1f5b89
eric <eric.jeangirard@recherche.gouv.fr>
2023-07-20 22:14:11 +0200

    fix upload s3

--- project/server/main/s3.py
+++ project/server/main/s3.py
@@ -19,5 +19,5 @@ def upload_object(container: str, source: str, destination: str) -> str:
     logger.debug(f'Uploading {source} in {container} as {destination}')
     data = open(f'{source}', 'rb')
   
-    client.put_object(Key=destination, Body=data, Bucket=container)
+    client.put_object(Key=destination, Body=data, Bucket=container, ACL='public-read')
     return f'ok: 1'
--- project/server/main/scanr.py
+++ project/server/main/scanr.py
@@ -122,6 +122,7 @@ def export_scanr(args):
     index_name = args.get('index')
     os.system(f'cd {MOUNTED_VOLUME}scanr && rm -rf persons.jsonl.gz && gzip persons.jsonl')
     upload_object(container='scanr-data', source = f'{MOUNTED_VOLUME}scanr/persons.jsonl.gz', destination='production/persons.jsonl.gz')
+    #upload_object(container='scanr-data', source = f'{MOUNTED_VOLUME}scanr/publications.jsonl.gz', destination='production/publications.jsonl.gz')
     #upload_sword(index_name)
 
 def export_one_person(idref, input_dict, ix):
